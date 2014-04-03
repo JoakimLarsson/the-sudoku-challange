c928cada5aceda0b2fca19505fbc4a81f613b102
Joakim Larsson <Joakim.Larsson@zenterio.com>
2014-04-03 17:46:10 +0200

    Cleaned up implementation file

--- yass-1.0.c
+++ yass-1.0.c
@@ -1,5 +1,5 @@
 //
-// Example implementation of solve_board() for the Sudoku Challange using Yass 1.0 by Binesh Bannerjee
+// Implementation of solve_board() for the Sudoku Challange using Yass 1.0 by Binesh Bannerjee
 //
 // GPL contaminated by design 
 //
@@ -15,43 +15,9 @@
 #include <stdio.h>
 #include "include/solver.h"
 
-#if DEBUG | DEBUG1
+extern "C" char *solver_name(){ return (char *) "Yass 1.0"; }
 
-// Test boards
-
-    // char board[] = "123456789123456789123456789123456789123456789123456789123456789123456789123456789";
-    // char board[] = "---------------------------------------------------------------------------------";
-    //char board[] = "---3--67-5--4--8--628---3-----2-4-37---------41-7-5-----2---961--6--1--4-84--9---"; // Time 207
-    //char board[] = "--71-6--24---9------128-63-2--315-6-93-47-----58---7-3----4752-6-5--19-------238-"; // Time: 25
-    //char board[] = "-17--3-6---2-8--546---2---9--31---7--8-9---2346------11--37-8----4-651--9--------"; // Time: 29
-    //char board[] = "3----87-------9--41---6---2--5-23-8--7---4-9--6---15--8---5---67--1-------98----5"; // Time 192 (2000 medium)
-    //char board[] = "-2-9--83---7--4--6--5--1-9--3-2----4--6-7-5--1---89-2--4-3--1--8--7--6---72--6-4-"; // Time 29 (2001 medium)
-    //char board[] = "-1---8-5--6--4-7-------2--9-3---1-8-7---6---2-5-97--4-8--3-------9-1--6--4-5---7-";   // Time 209 (2125 medium)
-    char board[] = "-9--3-6------7-2----48----51--5---8---7---9---3---6--42----17----8-2------6-5--9-";   // Left 44  (2151 medium)
-    //char board[] = "8--6----2-4--5--1----7----3-9---4--62-------87---1--5-3----9----1--8--9-4----2--5"; // Left 56 (insane)
-    // char board[] = "-6---39--5--1-----8-------7-4-2--6--7-------8--3--9-1-2-------5-----4--3--87---2-"; // Left 43 (hard)
-
-
-
-void print_board(char *board)
-{
-    int a, b;
-
-    printf("\n\n");
-    for (a = 0; a < 9; a++)
-    {
-        for (b = 0; b < 9; b++)
-        {
-            printf("%c ",board[a * 9 + b]);
-        }    
-        printf("\n");
-    }
-}
-#endif
-
-char *solver_name(){ return "Yass 1.0"; }
-
-void solve_board(char *board)
+extern "C" void solve_board(char *board)
 {
     int i, j;
     int yboard[9][9];
@@ -72,16 +38,3 @@ void solve_board(char *board)
     }
 
 }
-
-#if DEBUG | DEBUG1
-
-main()
-{
-    print_board(board);
-    solve_board(board);
-    print_board(board);
-
-//    exit(0);
-}
-
-#endif
