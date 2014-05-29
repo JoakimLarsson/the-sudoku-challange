da0cac50ac01fafd5837e411b0316cbcb2639d85
U-ZENTERIO\jolar <jolar@STOW0004.zenterio.lan>
2014-05-29 14:39:40 +0200

    Added naked pair reduction in row and sqr, new debug level and global to local pos in sqr translation

--- solve.c
+++ solve.c
@@ -7,9 +7,11 @@
 #include <stdlib.h>
 
 #ifdef DEBUG
-#define DEBUG3(x)  x
+#define DEBUG3(x) x
+#define DEBUG4(x) 
 #else
 #define DEBUG3(x)  /* */
+#define DEBUG4(x)  /* */
 #endif
 
 #if DEBUG | DEBUG1
@@ -44,6 +46,26 @@ static void print_board(char *board)
         }    
         printf("\n");
     }
+}
+
+static inline void print_candidates(int *candidates)
+{
+  printf("Candidates: "); for (int i = 0; i < 81; i++) printf("%s%03x", (i % 9) == 0 ? "\n     " : " ", candidates[i]); printf("\n");
+  //  printf("Candidates:\n");
+  printf("------+-----+-----+-----+-----+-----+-----+-----+-----+\n");
+  for (int l = 0; l < 9; l++){
+    for (int i = 0; i < 3; i++){
+      printf("| ");
+      for (int j = 0; j < 9; j++){
+	for (int k = 0; k < 3; k++){
+	  printf("%c", ~candidates[j + 9 * l] & (1 << (k + i * 3)) ? '.' : '0' + k + i * 3 + 1);
+	}
+	printf(" | ");
+      }
+      printf("\n");
+    }
+    printf("+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n");
+  }
 }
 #endif
 
@@ -103,6 +125,9 @@ extern "C" void solve_board(char *board)
 // Cell  (0-81) to what square (0-8)
 char sqrs[]     = "000111222000111222000111222333444555333444555333444555666777888666777888666777888";
 
+// Cell  (0-81) to what position in square (0-8)
+char sqrp[]     = "012012012345345345678678678012012012345345345678678678012012012345345345678678678";
+
 // pos (0-8) in square (0-8) converted to global pos (0-81)
 int sqrtopos[9][9] = {
                     { 0,  1,  2,  9, 10, 11, 18, 19, 20},
@@ -147,6 +172,7 @@ char n2[]    = "201534867";
     int ci, ri;
 
     for (i = 0; i < sizeof(sqrs); i++){ sqrs[i] -= (int) '0'; }
+    for (i = 0; i < sizeof(sqrp); i++){ sqrp[i] -= (int) '0'; }
     //    for (i = 0; i < sizeof(sqri); i++){ sqri[i] -= (int) '0'; }
     for (i = 0; i < sizeof(cols); i++){ cols[i] -= (int) '0'; }
     for (i = 0; i < sizeof(rows); i++){ rows[i] -= (int) '0'; }
@@ -160,6 +186,7 @@ char n2[]    = "201534867";
     for (i = 0; i < 512; i++)
     {
       countbits[i] = (i & 1) + countbits[i / 2];
+      //      printf("%03x:%d%s", i, countbits[i], ((i + 1) % 16) == 0 ? "\n" : " ");
     }
 
     DEBUG3(print_board(board);)
@@ -199,6 +226,8 @@ char n2[]    = "201534867";
       }
     }
 
+    DEBUG3(print_candidates(cnb););
+
     fnd = 1;
     type = 0;
 
@@ -212,18 +241,17 @@ char n2[]    = "201534867";
 #define NAKED_PAIR_IN_ROW  4
 #define NAKED_PAIR_IN_SQR  5
 
-    while (fnd > 0 || type < NAKED_PAIR_IN_COL)
+    while (fnd > 0 || type < NAKED_PAIR_IN_SQR)
     {
-      DEBUG3(print_board(board););
-      DEBUG3(printf("Type:%d Fnd:%d\n", type, fnd););
+      DEBUG4(print_board(board););
       type = (fnd == 0) ? type + 1 : 0;
       fnd = 0;
-      DEBUG3(printf("Type:%d Fnd:%d\n", type, fnd););
 
       DEBUG3(printf("col: "); for (i = 0; i < 9; i++) printf("%03x ", col[i]); printf("\n"););
       DEBUG3(printf("row: "); for (i = 0; i < 9; i++) printf("%03x ", row[i]); printf("\n"););
       DEBUG3(printf("sqr: "); for (i = 0; i < 9; i++) printf("%03x ", sqr[i]); printf("\n"););
       DEBUG3(printf("cnb: "); for (i = 0; i < 81; i++) printf("%s%03x", (i % 9) == 0 ? "\n     " : " ", cnb[i]); printf("\n"););
+      DEBUG3(print_candidates(cnb););
       DEBUG3(printf("Searching..using %d\n", type););
 
       for (ci = 0; ci < 9; ci++)
@@ -288,11 +316,11 @@ char n2[]    = "201534867";
 		{
 		  for (k = 0; k < 9; k++)
 		  {
-		    if (k != ri && countbits[cnb[k * 9 + ci]] == 2 && cnb[k * 9 + ci] == cnb[ri * 9 + ci])
+		    if (k != ri && countbits[cnb[k * 9 + ci] & 0x1ff] == 2 && cnb[k * 9 + ci] == cnb[ri * 9 + ci])
 		    {
 		      int numbit;
 
-		      printf("Found %03x in both (%d/%d) and (%d/%d)\n", cnb[ri * 9 + ci], ri, ci, k, ci);
+		      DEBUG3(printf("Found %03x in both (%d/%d) and (%d/%d)\n", cnb[ri * 9 + ci], ri, ci, k, ci););
 		      /* Update scratchpad */
 		      numbit = ~cnb[ri * 9 + ci] & 0x3ff; // Make mask of bit position and preserve solved bit (0x200)
 		      for (int r = 0; r < 9; r++)
@@ -313,9 +341,74 @@ char n2[]    = "201534867";
 		break;
 	      case NAKED_PAIR_IN_ROW:
 		/* Find to positions in the same row with the same single two candidates */
+		if (countbits[cnb[ri * 9 + ci]] == 2)
+		{
+		  for (k = 0; k < 9; k++)
+		  {
+		    if (k != ci && countbits[cnb[ri * 9 + k] & 0x1ff] == 2 && cnb[ri * 9 + k] == cnb[ri * 9 + ci])
+		    {
+		      int numbit;
+
+		      DEBUG3(printf("Found %03x in both (%d/%d) and (%d/%d)\n", cnb[ri * 9 + ci], ri, ci, ri, k););
+		      /* Update scratchpad */
+		      numbit = ~cnb[ri * 9 + ci] & 0x3ff; // Make mask of bit position and preserve solved bit (0x200)
+		      for (int c = 0; c < 9; c++)
+		      {
+			if (c != k && c != ci)
+			{
+			  fnd = fnd + (cnb[ri  *  9 + c] & (~numbit & 0x1ff)) ? 1 : 0;
+			  DEBUG3(printf("FND:%d\n", fnd););
+			  cnb[ri  *  9 + c] &= numbit; // clear candidates in same row as pos
+			}
+		      }
+		      //		      type = 0;
+		      //		      fnd++;
+		      DEBUG3(printf("Cnb: "); for (i = 0; i < 81; i++) printf("%s%03x", (i % 9) == 0 ? "\n     " : " ", cnb[i]); printf("\n"););
+		    }
+		  }  
+		}
 		break;
 	      case NAKED_PAIR_IN_SQR:
 		/* Find to positions in the same square with the same single two candidates */
+		if (countbits[cnb[ri * 9 + ci]] == 2)
+		{
+		  DEBUG3(printf("Checking square with pos %d\n", ri * 9 + ci););
+		  for (k = 0; k < 9; k++)
+		  {
+		    DEBUG4(printf("ref pos %d -> square %d + k -> pos %d\n", ri * 9 + ci, sqrs[ri * 9 + ci], sqrtopos[sqrs[ri * 9 + ci]][k]););
+		    if (ri * 9 + ci != sqrtopos[sqrs[ri * 9 + ci]][k] &&                   // Not the same square 
+			countbits[cnb[sqrtopos[sqrs[ri * 9 + ci]][k]] & 0x1ff] == 2 &&  // same bitcount
+			cnb[sqrtopos[sqrs[ri * 9 + ci]][k]] == cnb[ri * 9 + ci])        // same bits 
+		    {
+		      int numbit;
+
+		      DEBUG3(printf("Found %03x in both (%d/%d) and (%d/%d), masking out candidates\n", cnb[ri * 9 + ci], ri, ci, rows[sqrtopos[sqrs[ri * 9 + ci]][k]], cols[sqrtopos[sqrs[ri * 9 + ci]][k]]););
+		      /* Update scratchpad */
+		      numbit = ~cnb[ri * 9 + ci] & 0x3ff; // Make mask of bit position and preserve solved bit (0x200)
+		      for (int s = 0; s < 9; s++) // Go over square to mask all but the two found positions
+		      {
+			DEBUG4(printf("  mask %03x for pos %d ", numbit,sqrtopos[sqrs[ri * 9 + ci]][s]););
+			/*			printf("s: %d k: %d ci: %d sqrs: %d - global pos ri ci: %d global pos k: %d", 
+			       s, 
+			       sqrp[sqrtopos[sqrs[ri * 9 + ci]][k]], 
+			            sqrp[sqrtopos[sqrs[ri * 9 + ci]][ci]], 
+				    sqrs[ri * 9 + ci], ri * 9 + ci, sqrtopos[sqrs[ri * 9 + ci]][k]); */
+			// if (s != sqrp[sqrtopos[sqrs[ri * 9 + ci]][k]] && s != sqrp[sqrtopos[sqrs[ri * 9 + ci]][ci]])
+			if (ri * 9 + ci != sqrtopos[sqrs[ri * 9 + ci]][s] && s != k)
+			{
+			  DEBUG4(printf(" ok\n");)
+			  fnd = fnd + (cnb[sqrtopos[sqrs[ri * 9 + ci]][s]] & (~numbit & 0x1ff)) ? 1 : 0;
+			  DEBUG3(printf("FND:%d\n", fnd););
+			  cnb[sqrtopos[sqrs[ri * 9 + ci]][s]] &= numbit; // clear candidates in same row as pos
+			}
+			DEBUG4(else printf(" not\n"););
+		      }
+		      //		      type = 0;
+		      //		      fnd++;
+		      DEBUG3(printf("Cnb: "); for (i = 0; i < 81; i++) printf("%s%03x", (i % 9) == 0 ? "\n     " : " ", cnb[i]); printf("\n"););
+		    }
+		  }  
+		}
 		break;
 	      default:
 		printf("Wrong strategy type %d\n", type);
